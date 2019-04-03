c73060042fac7be847d1abdef95673afffba2218
fenouil <fenouil@RFDELL>
2019-04-03 17:09:39 +0200

    Added friendly messages in log for tunnel up and down scripts

--- openvpn/tunnelDown.sh
+++ openvpn/tunnelDown.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "tunnelDown: Tunnel is down, stopping transmission and tinyproxy..."
+
 /importedScripts/transmission/stop_transmission.sh
 /importedScripts/tinyproxy/stop_tinyproxy.sh
+
+echo "tunnelDown: transmission and tinyproxy shutdown scripts executed :(" 
--- openvpn/tunnelUp.sh
+++ openvpn/tunnelUp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "tunnelUp: Tunnel is up, starting transmission and tinyproxy..."
+
 /importedScripts/transmission/start_transmission.sh "$@"
 /importedScripts/tinyproxy/start_tinyproxy.sh
+
+echo "tunnelUp: transmission and tinyproxy startup scripts executed, external IP is : $(curl -s https://ipecho.net/plain)" 
