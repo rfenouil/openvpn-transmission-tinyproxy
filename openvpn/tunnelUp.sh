d1ff6b5a63d889fc34703533ebc42948939fb39d
fenouil <fenouil@RFDELL>
2019-04-03 17:23:38 +0200

    test reporting external IP in logs

--- openvpn/tunnelUp.sh
+++ openvpn/tunnelUp.sh
@@ -5,4 +5,4 @@ echo "tunnelUp: Tunnel is up, starting transmission and tinyproxy..."
 /importedScripts/transmission/start_transmission.sh "$@"
 /importedScripts/tinyproxy/start_tinyproxy.sh
 
-echo "tunnelUp: transmission and tinyproxy startup scripts executed, external IP is : $(curl -s https://ipecho.net/plain)" 
+echo "tunnelUp: transmission and tinyproxy startup scripts executed, external IP is: $(curl -s ipecho.net/plain)" 
